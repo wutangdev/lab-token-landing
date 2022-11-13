0bb1251e902fb5b54bf5a254e1788be867e819d7
LordNikon <100640945+wutangdev@users.noreply.github.com>
2022-11-13 18:33:29 -0500

    apos fix

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -33,7 +33,7 @@ function About() {
                     </div>
                     <div className="col-lg-6">
                         <h1 className="display-5 fw-bold lh-1 mb-3">How does it Work?</h1>
-                        <p className="lead">Lab Token's initial supply will be 20% of the max supply. The remaining supply will be protocol governed and used to grow our decentralized ecosystem. Users will be able to borrow, lend, and trade their assets within the Lab Reserve platform without worry of centralized oversight. Investors of Lab token will be able to wrap their LAB into gLAB to vote on and propose changes to the protocol. </p>
+                        <p className="lead">Lab Token{"'"}s initial supply will be 20% of the max supply. The remaining supply will be protocol governed and used to grow our decentralized ecosystem. Users will be able to borrow, lend, and trade their assets within the Lab Reserve platform without worry of centralized oversight. Investors of Lab token will be able to wrap their LAB into gLAB to vote on and propose changes to the protocol. </p>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-start">
                         </div>
                     </div>
